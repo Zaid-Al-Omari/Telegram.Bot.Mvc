7859c991971a6f05f1736f0dd3b0ab01594182f7
Zaid Al-Omari <e0013259@u.nus.edu>
2017-08-27 09:11:09 +0800

    Chat Session Key Accessor

--- Telegram.Bot.Mvc/Framework/ChatSession.cs
+++ Telegram.Bot.Mvc/Framework/ChatSession.cs
@@ -1,4 +1,6 @@
-﻿using System.Dynamic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
 using Telegram.Bot.Types;
 
 namespace Telegram.Bot.Mvc.Framework {
@@ -6,9 +8,15 @@ namespace Telegram.Bot.Mvc.Framework {
 
         public ChatSession(ChatId chatId) {
             ChatId = chatId;
+            Clear();
         }
         public ChatId ChatId { get; set; }
-        public dynamic Bag { get; protected set; } = new ExpandoObject();
+        
+
+        public void Clear()
+        {
+            _bag = new Dictionary<string, object>();
+        }
 
         public override bool Equals(object obj) {
             if (obj as ChatSession == null) return false;
@@ -17,6 +25,21 @@ namespace Telegram.Bot.Mvc.Framework {
 
         public override int GetHashCode() {
             return ChatId.GetHashCode();
+        }
+
+        private IDictionary<string, object> _bag;
+
+        public object this[string key]
+        {
+            get
+            {
+                _bag.TryGetValue(key, out object result);
+                return result;
+            }
+            set
+            {
+                _bag[key] = value;
+            }
         }
     }
 }
